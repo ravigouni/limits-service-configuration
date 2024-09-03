34bda17ea5730ae4126b677a4ef44efc17af43eb
unknown <ravi2021.gouni@gmail.com>
2024-09-03 10:06:40 +0530

    Adding cloud configs

---
+++ src/main/java/com/ravi/springcloud/spring_config_cloud/SpringConfigCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.springcloud.spring_config_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigCloudApplication.class, args);
+	}
+
+}
